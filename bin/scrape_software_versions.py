0b2326bb3de580dd432b44463ea1c4ae1ebf54cc
drpatelh <drhpatel@gmail.com>
2018-10-01 14:01:06 +0100

    add custom software

--- bin/scrape_software_versions.py
+++ bin/scrape_software_versions.py
@@ -7,12 +7,35 @@ regexes = {
     'nf-core/atacseq': ['v_pipeline.txt', r"(\S+)"],
     'Nextflow': ['v_nextflow.txt', r"(\S+)"],
     'FastQC': ['v_fastqc.txt', r"FastQC v(\S+)"],
+    'Trim Galore!': ['v_trim_galore.txt', r"version (\S+)"],
+    'BWA': ['v_bwa.txt', r"Version: (\S+)"],
+    'Samtools': ['v_samtools.txt', r"samtools (\S+)"],
+    'BEDTools': ['v_bedtools.txt', r"bedtools v(\S+)"],
+    'BamTools': ['v_bamtools.txt', r"bamtools (\S+)"],
+    'Picard': ['v_picard.txt', r"version (\S+)"],
+    'R': ['v_R.txt', r"R version (\S+)"],
+    'Pysam': ['v_pysam.txt', r"(\S+)"],
+    'MACS2': ['v_macs2.txt', r"macs2 (\S+)"],
+    'HOMER': ['v_homer.txt', r"(\S+)"],
+    'featureCounts': ['v_featurecounts.txt', r"featureCounts v(\S+)"],
     'MultiQC': ['v_multiqc.txt', r"multiqc, version (\S+)"],
 }
+
 results = OrderedDict()
 results['nf-core/atacseq'] = '<span style="color:#999999;\">N/A</span>'
 results['Nextflow'] = '<span style="color:#999999;\">N/A</span>'
 results['FastQC'] = '<span style="color:#999999;\">N/A</span>'
+results['Trim Galore!'] = '<span style="color:#999999;\">N/A</span>'
+results['BWA'] = '<span style="color:#999999;\">N/A</span>'
+results['Samtools'] = '<span style="color:#999999;\">N/A</span>'
+results['BEDTools'] = '<span style="color:#999999;\">N/A</span>'
+results['BamTools'] = '<span style="color:#999999;\">N/A</span>'
+results['Picard'] = '<span style="color:#999999;\">N/A</span>'
+results['R'] = '<span style="color:#999999;\">N/A</span>'
+results['Pysam'] = '<span style="color:#999999;\">N/A</span>'
+results['MACS2'] = '<span style="color:#999999;\">N/A</span>'
+results['HOMER'] = '<span style="color:#999999;\">N/A</span>'
+results['featureCounts'] = '<span style="color:#999999;\">N/A</span>'
 results['MultiQC'] = '<span style="color:#999999;\">N/A</span>'
 
 # Search each file using its regex
