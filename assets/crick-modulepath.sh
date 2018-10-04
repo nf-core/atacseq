516ebfef6a89af66726a29ae7a34586ff4c35921
drpatelh <drhpatel@gmail.com>
2018-10-04 12:32:56 +0100

    initial commit

---
+++ assets/crick-modulepath.sh
@@ -0,0 +1,11 @@
+
+module unuse /camp/stp/babs/working/software/modules/all
+module unuse /camp/stp/babs/working/software/eb/modules/all
+
+module use /camp/apps/eb/dev/modules/all
+module use /camp/apps/eb/intel-2017a/modules/all
+module use /camp/apps/eb/modules/all
+ 
+module use /camp/apps/misc/stp/babs/manual/modules/all
+module use /camp/apps/misc/stp/babs/easybuild/modules/all
+
