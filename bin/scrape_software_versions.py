277e497219ed7345214cc71737e66425dc66f83c
drpatelh <drhpatel@gmail.com>
2018-10-09 16:34:41 +0100

    Add deeptools version parsing

--- bin/scrape_software_versions.py
+++ bin/scrape_software_versions.py
@@ -18,6 +18,7 @@ regexes = {
     'MACS2': ['v_macs2.txt', r"macs2 (\S+)"],
     'HOMER': ['v_homer.txt', r"(\S+)"],
     'featureCounts': ['v_featurecounts.txt', r"featureCounts v(\S+)"],
+    'deepTools': ['v_deeptools.txt', r"computeMatrix (\S+)"],
     'MultiQC': ['v_multiqc.txt', r"multiqc, version (\S+)"],
 }
 
@@ -36,6 +37,7 @@ results['Pysam'] = '<span style="color:#999999;\">N/A</span>'
 results['MACS2'] = '<span style="color:#999999;\">N/A</span>'
 results['HOMER'] = '<span style="color:#999999;\">N/A</span>'
 results['featureCounts'] = '<span style="color:#999999;\">N/A</span>'
+results['deepTools'] = '<span style="color:#999999;\">N/A</span>'
 results['MultiQC'] = '<span style="color:#999999;\">N/A</span>'
 
 # Search each file using its regex
