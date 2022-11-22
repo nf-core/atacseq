0edb11baf62a28c57588d09094e049adbb12c7d4
bjlang <>
2022-11-22 17:39:36 +0100

    Add check for multiple replicates having the same data type

--- bin/check_samplesheet.py
+++ bin/check_samplesheet.py
@@ -137,13 +137,24 @@ def check_samplesheet(file_in, file_out):
 
                 ## Check that replicate ids are in format 1..<num_replicates>
                 uniq_rep_ids = sorted(list(set(sample_mapping_dict[sample].keys())))
-                if len(uniq_rep_ids) != max(uniq_rep_ids) or 1 != min(uniq_rep_ids) :
+                if len(uniq_rep_ids) != max(uniq_rep_ids) or 1 != min(uniq_rep_ids):
                     print_error(
                         "Replicate ids must start with 1..<num_replicates>!",
                         "Sample",
                         "{}, replicate ids: {}".format(sample, ",".join([str(x) for x in uniq_rep_ids])),
                     )
                     sys.exit(1)
+
+                ## Check that multiple runs of the same sample are of the same datatype i.e. single-end / paired-end
+                if not all(
+                    x[0][3] == sample_mapping_dict[sample][1][0][3]
+                    for x in sample_mapping_dict[sample].values()
+                ):
+                    print_error(
+                        f"Multiple replicates of a sample must be of the same datatype i.e. single-end or paired-end!",
+                        "Sample",
+                        sample,
+                    )
 
                 for replicate in sorted(sample_mapping_dict[sample].keys()):
                     ## Check that multiple runs of the same sample are of the same datatype i.e. single-end / paired-end
