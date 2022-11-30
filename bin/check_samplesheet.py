128c571e2ad17f33991bb3974873f39c92747481
Harshil Patel <drpatelhh@gmail.com>
2022-11-30 12:51:09 +0000

    Fixes from PR 245

--- bin/check_samplesheet.py
+++ bin/check_samplesheet.py
@@ -146,14 +146,24 @@ def check_samplesheet(file_in, file_out):
                     )
                     sys.exit(1)
 
+                ## Check that multiple replicates are of the same datatype i.e. single-end / paired-end
+                if not all(
+                    x[0][3] == sample_mapping_dict[sample][1][0][3] for x in sample_mapping_dict[sample].values()
+                ):
+                    print_error(
+                        f"Multiple replicates of a sample must be of the same datatype i.e. single-end or paired-end!",
+                        "Sample",
+                        sample,
+                    )
+
                 for replicate in sorted(sample_mapping_dict[sample].keys()):
-                    ## Check that multiple replicates are of the same datatype i.e. single-end / paired-end
+                    ## Check that multiple runs of the same sample are of the same datatype i.e. single-end / paired-end
                     if not all(
                         x[3] == sample_mapping_dict[sample][replicate][0][3]
                         for x in sample_mapping_dict[sample][replicate]
                     ):
                         print_error(
-                            f"Multiple replicates of a sample must be of the same datatype i.e. single-end or paired-end!",
+                            f"Multiple runs of a sample must be of the same datatype i.e. single-end or paired-end!",
                             "Sample",
                             sample,
                         )
